89a18ef23b83344bda417903fa3ce58f97fc1811
Facedogs <39603914@qq.com>
2016-08-29 17:55:02 +0000

    login

--- login_interface.py
+++ login_interface.py
@@ -2,6 +2,24 @@
 # coding=utf-8
 import getpass
 
-username = input("Please enter the username:")
-password = getpass.getpass("Please enter the password:")
+# username = raw_input("Please enter the username:")
+# papssword = getpass.getpass("Please enter the password:")
+
+# print username
+# print password
+user = "yinan"
+pwd = "13197"
+
+run_forver = True
+
+# while run_forver:
+count = 0
+for count in range(3):
+    username = raw_input("Please enter the username:")
+    password = getpass.getpass("Please enter the password:")
+    if username == user and password == pwd:
+         print("ok,You are right!")
+         break
+    else:
+         print("try again!")
 
