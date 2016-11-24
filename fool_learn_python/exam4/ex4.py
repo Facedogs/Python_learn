384c48832033226e40b2249d535dec3d4f6c4400
Facedogs <39603914@qq.com>
2016-11-24 17:10:27 +0000

    习题四，变量的使用（修改为python3代码）

--- fool_learn_python/exam4/ex4.py
+++ fool_learn_python/exam4/ex4.py
@@ -11,9 +11,9 @@ carpool_capacity = cars_driver * space_in_car
 average_passengers_per_car = passenger / cars_driver
 
 
-print "There are",cars,"cars available"
-print "There are only ",drivers,"drivers available."
-print "There will be ",cars_not_driven,"empty cars today."
-print "We can transport",carpool_capacity,"people today."
-print "We have",passenger,"to carpool today."
-print "We need to put about",average_passengers_per_car,"in each car."
+print ("There are",cars,"cars available")
+print ("There are only ",drivers,"drivers available.")
+print ("There will be ",cars_not_driven,"empty cars today.")
+print ("We can transport",carpool_capacity,"people today.")
+print ("We have",passenger,"to carpool today.")
+print ("We need to put about",average_passengers_per_car,"in each car.")
