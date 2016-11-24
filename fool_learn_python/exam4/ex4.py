bcd05e0cfebfe036d001951d5a496dcc068360ea
Facedogs <39603914@qq.com>
2016-11-24 16:53:28 +0000

    习题四，变量的命名结合运算使用

---
+++ fool_learn_python/exam4/ex4.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+cars = 100
+space_in_car = 4.0
+drivers = 30
+passenger = 90
+cars_not_driven = cars - drivers
+cars_driver = drivers
+carpool_capacity = cars_driver * space_in_car
+average_passengers_per_car = passenger / cars_driver
+
+
+print "There are",cars,"cars available"
+print "There are only ",drivers,"drivers available."
+print "There will be ",cars_not_driven,"empty cars today."
+print "We can transport",carpool_capacity,"people today."
+print "We have",passenger,"to carpool today."
+print "We need to put about",average_passengers_per_car,"in each car."
